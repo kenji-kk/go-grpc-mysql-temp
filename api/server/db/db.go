b8b71c2b635dcf08ebc7b9ed75e2a88a5e78a9fc
kenji <kenjix12x22@gmail.com>
2022-03-10 18:07:20 +0900

    db.goを作成

---
+++ api/server/db/db.go
@@ -0,0 +1,60 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+)
+
+var Db *sql.DB
+var err error 
+
+func DbConnect() {
+	Db, err := sql.Open("mysql", "go_grpc:password@tcp(mysql:3306)/go_database?charset=utf8parseTime=true&loc=Asia%2FTokyo")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("db接続完了")
+
+	cmdC := `CREATE TABLR IF NOT EXISTS country {
+		id int(11) NOT NULL AUTO_INCREMENT,
+		name varchar(255))`
+
+		_, err = Db.Exec(cmdC)
+		count := 0
+		if err != nil {
+			for {
+				if err == nil {
+					fmt.Println("CREATE成功")
+					break
+				}
+				fmt.Print(".")
+				time.Sleep(time.Second)
+				count++
+				if count > 100 {
+					fmt.Println("")
+					panic(err)
+				}
+				_, err = Db.Exec(cmdC)
+			}
+		}
+
+		cmd := `INSERT INTO country(name) VALUES(?)`
+		_, err = Db.Exec(cmd, "Japan")
+		if err != nil {
+			log.Fatalln((err))
+		}
+
+		cmd = `INSERT INTO country(name) VALUES(?)`
+		_, err = Db.Exec(cmd, "China")
+		if err != nil {
+			log.Fatalln((err))
+		}
+
+		cmd = `INSERT INTO country(name) VALUES(?)`
+		_, err = Db.Exec(cmd, "America")
+		if err != nil {
+			log.Fatalln((err))
+		}
+}
