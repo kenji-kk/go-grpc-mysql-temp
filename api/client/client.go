b391d6deb259892810c3e507b99815d3882443dd
kenji <kenjix12x22@gmail.com>
2022-03-10 17:46:06 +0900

    client.goを作成

---
+++ api/client/client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"app/pb"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"google.golang.org/grpc"
+)
+
+func checkErr(message string, err error) {
+	if err != nil {
+		log.Fatalf(message, err)
+	}
+}
+
+func country(w http.ResponseWriter, r *http.Request){
+	opts := grpc.WithInsecure()
+	cc, err := grpc.Dial("server:50051", opts)
+	checkErr("could not connect: %v", err)
+	defer cc.Close()
+	c := pb.NewAppServiceClient(cc)
+
+	res, err := c.GetAllCountry(context.Background(), &pb.AllCountryRequest{})
+	checkErr("Fail to create client: %v",err)
+	fmt.Printf("AllCountry has geted: %v", res)
+}
+
+func main() {
+	http.HandleFunc("/country", country)
+	http.ListenAndServe(":8080", nil)
+}
